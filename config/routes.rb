6b8ee8726d35f291be4e9b167374a5f482b05166
William Sinn <w.h.sinn@gmail.com>
2019-08-07 21:48:52 -0400

    add user render

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/show'
-  get 'projects/new'
-  get 'projects/create'
-  get 'projects/edit'
-  get 'projects/update'
-  get 'projects/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+  get 'users/:id', to 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
