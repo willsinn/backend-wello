3c5cd747a7514f7c18fbe70535134b3f21fda339
William Sinn <w.h.sinn@gmail.com>
2019-08-15 06:46:57 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'cards/create'
-  get 'cards/update'
-  get 'cards/destroy'
-  get '/user/:id', to: 'users#show'
+
 
   get 'user/:id/projects', to: 'projects#index'
   post 'user/:id/projects/new', to: 'projects#create'
@@ -15,5 +12,10 @@ Rails.application.routes.draw do
   post 'project/:id/items/new', to: 'items#create'
   post 'items/:id/delete',to: 'items#destroy'
 
+
+  get 'item/:id/cards/new', to: 'cards#create'
+  get 'cards/update'
+  get 'cards/:id/delete', to: 'cards#destroy'
+  get '/user/:id', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
