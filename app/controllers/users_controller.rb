3e9dd2470567ce4964d81bf8e998fc8da0eaed25
William Sinn <w.h.sinn@gmail.com>
2019-09-09 21:48:21 -0400

    add user create and private params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user
   end
-
+  def create
+    user = Project.create(user_params)
+    render json: user
+  end
+  private
+    def user_params
+      params.permit(:name)
+    end
 end
