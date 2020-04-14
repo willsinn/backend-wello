baad208f2dcaa548de8f433d055bb299b2daee86
William Sinn <w.h.sinn@gmail.com>
2020-04-14 09:59:45 -0400

    change label columns

--- db/migrate/20200414050228_create_labels.rb
+++ db/migrate/20200414050228_create_labels.rb
@@ -1,12 +1,8 @@
 class CreateLabels < ActiveRecord::Migration[5.2]
   def change
     create_table :labels do |t|
-      t.string :green, default: NULL
-      t.string :yellow, default: NULL
-      t.string :orange, default: NULL
-      t.string :red, default: NULL
-      t.string :purple, default: NULL
-      t.string :blue, default: NULL
+      t.string :name
+      t.string :color
 
       t.timestamps
     end
