8935f15c02e435b302cb5604543d3414d051ac6d
William Sinn <w.h.sinn@gmail.com>
2020-04-25 12:22:43 -0400

    rename task_checklists to checklists, this version renders and performs all fns previoiusly avail

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,4 +1,4 @@
-class TaskChecklist < ApplicationRecord
+class Checklist < ApplicationRecord
     belongs_to :task
     has_many :task_checklist_items
 end
--- app/models/task_checklist_item.rb
+++ app/models/task_checklist_item.rb
@@ -1,3 +1,3 @@
 class TaskChecklistItem < ApplicationRecord
-    belongs_to :task_checklist
+    belongs_to :checklist
 end
