ca76daae81e99479febd915733c81ed7572a7fd8
William Sinn <w.h.sinn@gmail.com>
2020-05-14 14:36:34 -0400

    add user auth, login/signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username, uniqueness: { case_sensitive: true }
+  
   has_many :boards
-
   has_many :user_boards
   has_many :boards, through: :user_boards
 end
