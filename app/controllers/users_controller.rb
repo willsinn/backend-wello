f24a84d9c2d3a745a78e8658ed6b28ef45c19f5e
William Sinn <w.h.sinn@gmail.com>
2020-05-15 18:03:52 -0400

    add user columns, model classes & controller columns, add bcrypt gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.permit(:name)
+      params.permit(:name, :email, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   has_many :boards
-
   has_many :user_boards
   has_many :boards, through: :user_boards
 end
