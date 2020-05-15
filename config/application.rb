314c6efb15ce39f345d5d960b467fee8e905a030
William Sinn <w.h.sinn@gmail.com>
2020-05-15 18:26:18 -0400

    add config.auto_load in config/application

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,12 @@ Bundler.require(*Rails.groups)
 module BackendWello
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
 
+    
+    # config.load_defaults 5.2
+    config.autoload_paths << Rails.root.join('lib')
+
+ 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
