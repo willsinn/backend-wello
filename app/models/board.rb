7655ae98f07e82837d72cb45c581c5f658ad1305
William Sinn <w.h.sinn@gmail.com>
2020-05-18 16:08:34 -0400

    fix signup endpoint

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,8 @@
 class Board < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+  belongs_to :owner, class_name: "User"
+
   belongs_to :user
 
   has_many :user_boards
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_232552) do
+ActiveRecord::Schema.define(version: 2020_05_17_222211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "accounts", force: :cascade do |t|
-    t.string "name"
-    t.bigint "owner_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-    t.index ["owner_id"], name: "index_accounts_on_owner_id"
-    t.index ["slug"], name: "index_accounts_on_slug", unique: true
-  end
 
   create_table "boards", force: :cascade do |t|
     t.bigint "user_id"
@@ -33,8 +23,11 @@ ActiveRecord::Schema.define(version: 2020_05_17_232552) do
     t.string "team_name"
     t.boolean "starred", default: false
     t.boolean "archived", default: false
+    t.bigint "owner_id"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_boards_on_owner_id"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
@@ -115,7 +108,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_232552) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "accounts", "users", column: "owner_id"
+  add_foreign_key "boards", "users", column: "owner_id"
   add_foreign_key "cards", "boards"
   add_foreign_key "checklists", "tasks"
   add_foreign_key "items", "checklists"
