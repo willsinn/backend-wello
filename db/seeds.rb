b75830c63a328788e39e43994e3d320ccab5f60e
William Sinn <w.h.sinn@gmail.com>
2020-06-13 14:00:04 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name:"Will")
+User.create(username: "ws", password_digest: "123456", name: "Will")
 
 Label.create(color:"green")
 Label.create(color:"yellow")
