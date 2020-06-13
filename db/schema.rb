144e3128e7c772cac639c3c9583f65b0430e33c7
William Sinn <w.h.sinn@gmail.com>
2020-06-13 12:07:52 -0400

    add username & password_digest to user table, remigrated and unchecked

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_073645) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
