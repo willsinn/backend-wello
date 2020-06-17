7dd4a0da54aaa4aed734e8800a94f3614d4a9102
William Sinn <w.h.sinn@gmail.com>
2020-06-17 14:21:27 -0400

    complete user-signup/login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,7 +14,6 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    byebug
     if @user.valid?
       @token = encode_token(user_id: @user.id)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
@@ -41,15 +40,3 @@ class Api::V1::UsersController < ApplicationController
     params.require(:user).permit(:email, :password, :name)
   end
 end
-# def show
-#   user = User.find(params[:id])
-#   render json: user
-# end
-# def create
-#   user = User.create(user_params)
-#   render json: user
-# end
-# private
-#   def user_params
-#     params.permit(:name)
-#   end
