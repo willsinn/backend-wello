5b883e780796f7385dcacdf3d8bd6eab0c3b8427
William Sinn <w.h.sinn@gmail.com>
2020-06-23 19:22:08 -0400

    update board update route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       get '/user/:id/boards', to: 'boards#index'
       post '/user/:id/boards/new', to: 'boards#create'
       get 'user/:id/board/:id', to: 'boards#show'
-      put '/board/:id/update', to: 'boards#update'
+      put '/board/update/:id', to: 'boards#update'
       # post '/user/:id/board/:id/delete', to: 'boards#destroy'
 
 
