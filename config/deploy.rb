84489bc9db577a39689b84b5f76f461d9d58cd3a
William Sinn <w.h.sinn@gmail.com>
2020-07-09 23:49:36 -0400

    update config/deploy && config/deploy/production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,18 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "backend-wello"
+set :repo_url, "git@github.com:willsinn/backend-Wello.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/backend-wello/code"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+set :keep_releases, 5
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '52.23.245.65', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
